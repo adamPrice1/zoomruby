97c2da24af8942c78a09e1ad92acb0f300fe6f20
Adam Price <adam.price@shopify.com>
2020-10-16 12:06:23 +0100

    part 1

---
+++ ascii.rb
@@ -0,0 +1,12 @@
+print "How big is your triangle? "
+input = gets.chomp
+until input.to_i.to_s == input
+  puts "try again: "
+  input = gets.chomp
+end
+
+input = input.to_i
+
+for i in 1..input
+  puts "*" * i
+end
