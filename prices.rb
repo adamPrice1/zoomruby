1f5c5d8fa3d13375c892129cb4cc350c95af8c09
Adam Price <adam.price@shopify.com>
2020-10-19 23:40:46 +0100

    pages 165

--- prices.rb
+++ prices.rb
@@ -1,36 +1,60 @@
-def total(prices)
-  amount = 0
+# def total(prices)
+#   amount = 0
+#   index = 0
+#   while index < prices.length
+#     amount += prices[index]
+#     index += 1
+#   end
+#   amount
+# end
+#
+# prices = [3.99, 25.00, 8.99]
+#
+# puts format("%.2f", total(prices))
+#
+# def refund(prices)
+#   amount = 0
+#   index = 0
+#   while index < prices.length
+#     amount -= prices[index]
+#     index += 1
+#   end
+#   amount
+# end
+#
+# puts format("%.2f", refund(prices))
+#
+# def show_discounts(prices)
+#   index = 0
+#   while index < prices.length
+#     amount_off = prices[index] / 3.0
+#     puts format("Your Discount: %.2f", amount_off)
+#     index += 1
+#   end
+# end
+#
+# show_discounts(prices)
+
+
+def do_something_with_every_item(array, operation)
+  if operation == "total" || operation == "refund"
+    amount = 0
+  end
   index = 0
-  while index < prices.length
-    amount += prices[index]
+
+  while index < array.length
+    if operation == "total"
+      amount += array[index]
+    elsif operation == "refund"
+      amount -= array[index]
+    elsif operation == "show discounts"
+      amount_off = array[index] / 3.0
+      puts format("Your Discount: %.2f", amount_off)
+    end
     index += 1
   end
-  amount
-end
 
-prices = [3.99, 25.00, 8.99]
-
-puts format("%.2f", total(prices))
-
-def refund(prices)
-  amount = 0
-  index = 0
-  while index < prices.length
-    amount -= prices[index]
-    index += 1
-  end
-  amount
-end
-
-puts format("%.2f", refund(prices))
-
-def show_discounts(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
-    puts format("Your Discount: %.2f", amount_off)
-    index += 1
+  if operation == "total" || operation == "refund"
+    return amount
   end
 end
-
-show_discounts(prices)
