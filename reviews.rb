61eb8fc537557b5161e1e21c33d3ad9d36e7174a
Adam Price <adam.price@shopify.com>
2020-10-26 01:00:08 +0000

    201-202

--- reviews.rb
+++ reviews.rb
@@ -1,6 +1,15 @@
 lines = []
+
 File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-puts lines.length
+relevant_lines = []
+
+lines.each do |line|
+  if line.include?("Truncated")
+    relevant_lines << line
+  end
+end
+
+puts relevant_lines
