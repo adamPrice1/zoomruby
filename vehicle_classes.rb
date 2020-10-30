210880929e1faeb25a1b3fbec6aab49749fd5098
Adam Price <adam.price@shopify.com>
2020-10-30 01:08:05 +0000

    78-80

--- vehicle_classes.rb
+++ vehicle_classes.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
 
   attr_accessor :odometer
   attr_accessor :gas_used
@@ -20,46 +20,22 @@ class Car
   end
 end
 
-class Truck
-
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-
-  def accelerate
-    puts "Floor it!"
-  end
-
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn two front wheels"
-  end
+class Car < Vehicle
 end
 
-class Motorcycle
-
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-
-  def accelerate
-    puts "Floor it!"
-  end
-
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn two front wheels"
-  end
+class Truck < Vehicle
 end
+
+class Motorcylce < Vehicle
+end
+
+truck = Truck.new
+truck.accelerate
+truck.steer
+
+car = Car.new
+car.odometer = 11432
+car.gas_used = 366
+
+puts "Lifetime Mileage"
+puts car.mileage
