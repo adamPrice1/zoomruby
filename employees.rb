47de3ac5318053e09a10bfc58902a1ed4560f71f
Adam Price <adam.price@shopify.com>
2020-10-30 02:31:55 +0000

    137-138

--- employees.rb
+++ employees.rb
@@ -29,3 +29,30 @@ salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
+
+class HourlyEmployee < Employee
+
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = @hourly_wage * @hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "pay this period: #{formatted_pay}"
+  end
+
+end
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 30
+hourly_employee.print_pay_stub
